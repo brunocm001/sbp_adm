e8ac9fd8ee158431492f04e4d5839bb22c4c401b
bruno WAMBO <brunowambo@gmail.com>
2025-07-13 06:30:43 +0200

    correction docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: 'static',
   server: {
     port: 3000,
     host: true
-  }
+  },
+  adapter: node({
+    mode: 'standalone' // ou 'middleware' si tu utilises Express
+  })
 }); 
